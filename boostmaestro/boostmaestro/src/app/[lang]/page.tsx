bacab3c38ac139fc0b944e65c26af5ffc45be73d
Nicolai Verheul <n.verheul@student.science.ru.nl>
2024-02-01 18:36:13 +0100

    Refactor Home component to use EditorWrapper

--- boostmaestro/boostmaestro/src/app/[lang]/page.tsx
+++ boostmaestro/boostmaestro/src/app/[lang]/page.tsx
@@ -1,17 +1,17 @@
 import { Locale } from '@/app/../../i18n.config'
-import HomeTopSection from '@/app/[lang]/components/HomeTopSection'
-import HomeOverview from '@/app/[lang]/components/HomeOverview'
+import EditorWrapper from '@/app/[lang]/components/editor/EditorWrapper'
 
 export default function Home({
-    params: { lang }
-  }: {
-    params: { lang: Locale }
-  }) {
-    
-    return (
-      <div className=''>
-        <HomeTopSection lang={lang} />
-        <HomeOverview lang={lang} />
+  params: { lang }
+}: {
+  params: { lang: Locale }
+}) {
+
+  return (
+    <div className='bg-background relative'>
+      <div className="relative container mx-auto px-4">
+        <EditorWrapper documentId='home-letter' initialLocale={lang} />
       </div>
+    </div>
   )
 }
